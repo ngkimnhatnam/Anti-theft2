bb4cc6f162d71f471fe0059b2a612e31e4a4de64
Nam Nguyen <ngkimnhatnam@gmail.com>
2018-02-13 16:35:25 +0000

    Final without auto bootup

--- final_antitheft.py
+++ final_antitheft.py
@@ -3,27 +3,54 @@ import serial
 import pynmea2
 import math
 import time
+import datetime
 import RPi.GPIO as GPIO
 import os
+import sys
+
+
 
 serialStream= serial.Serial("/dev/ttyS0",9600,timeout=0.5)
 coord1=[]                       #Coordinate list of trailing mode
 coord2=[]                       #Coordinate list of guarding mode
 collected_dist=[]
-buzzer=40                       #Position of buzzer signal pin on Pi
-buttonPin = 11                  #Position of button signal pin on Pi
-buttonPin2=37
 buzzer_parameter=[]
+
+buzzer=40                       #Position of buzzer signal pin on Pi
+buttonPin = 11                  #Position of button1 signal pin on Pi
+buttonPin2=37                   #Position of buzzer 2 on Pi
+bluepin=29                      #Position of blue pin on Pi
+redpin=31                       #Position of red pin on Pi
+greenpin=33                     #Position of green pin on Pi
+
+
 GPIO.setmode(GPIO.BOARD)
+
+#Buzzer and button settings
 GPIO.setup(buzzer, GPIO.OUT)
-GPIO.setup(buttonPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button status now ON
+GPIO.setup(buttonPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(buttonPin2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(buttonPin,GPIO.FALLING)
 GPIO.add_event_detect(buttonPin2,GPIO.FALLING)
+
+#LED settings
+GPIO.setup(bluepin, GPIO.OUT)
+GPIO.setup(redpin, GPIO.OUT)
+GPIO.setup(greenpin, GPIO.OUT)
+GPIO.output(bluepin, GPIO.LOW)
+GPIO.output(redpin, GPIO.LOW)
+GPIO.output(greenpin, GPIO.LOW)
+
 GPIO.setwarnings(False)
 
+ts=time.time()
+st=datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
 def trailing_mode(): #Calculate total distance travelled so far
-        f=open("BikingDistance2.txt", "w+")#Create a txt file to write data to
+        f=open("BikingDistance.txt", "a+")#Create a txt file to write data to
+        greenOff()
+        redOff()
+        blue()
         while GPIO.input(buttonPin)==1:
 
                 if len(coord1)<4:
@@ -31,11 +58,11 @@ def trailing_mode(): #Calculate total distance travelled so far
                 
                 elif len(coord1)==4:
                         haversine(coord1[0],coord1[1],coord1[2],coord1[3])
-                        del coord1[:]
+                        del coord1[0:2]
                         total_dist=round(sum(collected_dist),4)
-                        f.writelines("Total distance is: "+str(total_dist)+" km\n")
+                        f.writelines(st+" Total distance is: "+str(total_dist)+" km\n")
                         print ("Total distance travelled is: ",total_dist," km")
-
+                        
 
 def current_location(): #Get current location and append to list
         sentence=serialStream.readline()
@@ -49,7 +76,7 @@ def current_location(): #Get current location and append to list
                 coord1.append(la)
                 print ("Current location: {lat} North,{lon} East".format(lat=data.latitude, lon=data.longitude))
                 print (coord1)
-                time.sleep(2)
+                time.sleep(5)
                 
                 
 
@@ -86,7 +113,9 @@ def haversine(lon1,lat1,lon2,lat2): #Haversine formula to calculate distance bet
     #print("Distance:",km," km")
 
 def guarding_mode(): #Calculate total distance travelled so far
-        
+        green()
+        blueOff()
+        redOff()
         while GPIO.input(buttonPin2)==1:
 
                 if len(coord2)<4:
@@ -94,15 +123,15 @@ def guarding_mode(): #Calculate total distance travelled so far
                 
                 elif len(coord2)==4:
                         haversine_frompos0(coord2[0],coord2[1],coord2[2],coord2[3])
-                        if buzzer_parameter[0]>20:
-                            buzzer()                         
+                        if buzzer_parameter[0]>3:
+                            greenOff()
+                            blueOff()
+                            buzzer()
+                            redOn()
                         del buzzer_parameter[:]
                         del coord2[2:4]
                         buzzer_off()
-                        
-                        #print("Highest margin of error: ",max(margin_error)," m")
-                        
-                
+                        redOff()
 
 def current_location2(): #Get current location and append to list
         sentence=serialStream.readline()
@@ -151,26 +180,61 @@ def haversine_frompos0(lon1,lat1,lon2,lat2): #Haversine formula to calculate dis
     
     #print("Distance:",km," km")
 
-def beep(): #Calling the buzzer to raise beeping alarm
+def beep(): #Beeping sound
     
-    for n in range(0,5):
+    for n in range(0,3):
         GPIO.output(40, GPIO.HIGH)
         time.sleep(.05)
         GPIO.output(40, GPIO.LOW)
         time.sleep(.05)
     
-def buzzer_off():
+def buzzer_off():#Turn off buzzer
         GPIO.output(40, GPIO.LOW)
 def buzzer():#Turn on buzzer with keyboard interrupt
-    n=0
-    for n in range(0,5):
+    for n in range(0,1):
         try:
             beep()
         except KeyboardInterrupt:
-            GPIO.output(40, GPIO.LOW)
+            buzzer_off()
+            
+def blueOn():#Blue light on
+    GPIO.output(bluepin, GPIO.HIGH)
+     
+def blue():
+        try:
+                blueOn()
+        except KeyboardInterrupt:
+                blueOff()
+                
+def blueOff():# Blue light off
+    GPIO.output(bluepin, GPIO.LOW)
+def redOn():#Red light on
+    for i in range (0,1):
+            GPIO.output(redpin, GPIO.HIGH)
+            time.sleep(.05)
+            GPIO.output(redpin, GPIO.LOW)
+            time.sleep(.05)
+def redOff():#Red light off
+    GPIO.output(redpin, GPIO.LOW)
+def red():
+        for n in range(0,5):
+                try:
+                        redOn()
+                except KeyboardInterrupt:
+                        redOff()
+def greenOn():#Green light on
+    GPIO.output(greenpin, GPIO.HIGH)
+def greenOff():#Green light off
+    GPIO.output(greenpin, GPIO.LOW)
+def green():
+        try:
+                greenOn()
+        except KeyboardInterrupt:
+                greenOff()
 
 def main():
         while True:
+                trailing_mode()
                 if GPIO.event_detected(buttonPin2):
                         trailing_mode()
                         GPIO.remove_event_detect(buttonPin2)
@@ -180,8 +244,9 @@ def main():
                         GPIO.remove_event_detect(buttonPin)
                         GPIO.add_event_detect(buttonPin, GPIO.RISING)
 
+if __name__ == '__main__':
+        main()
 
-main()
 
         
         
